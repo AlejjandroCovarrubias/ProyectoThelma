7b9724ac170d30f9676d45ada57bc2aa86f2d311
Esauu <esau.gonzalez0277@alumnos.udg.mx>
2024-05-18 15:42:09 -0600

    Se agregó el botón de reportar recetaZ

--- resources/views/recetas/mireceta.blade.php
+++ resources/views/recetas/mireceta.blade.php
@@ -1,122 +1,121 @@
 <x-Layout titulo="Prueba">
-<div class="container-fluid py-5 mt-5">
-<div class="container-fluid py-5 mt-5">
-    <div class="container py-5">
-        <div class="row g-4 mb-5">
-            <div class="col-lg-12">
-                <div class="row g-4">
-                    <div class="col-lg-4">
-                        <div class="border rounded">
-                            <a href="#">
-                                <img src="img/single-item.jpg" class="img-fluid rounded" alt="Image">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="d-flex justify-content-between align-items-center">
-                            <h4 class="fw-bold mb-3">{{$recipe->title_recipe}}</h4>
-                            
-                            <div class="icon-container">
-                                <i class="fas fa-heart icon"></i>
-                                <i class="fas fa-share-alt icon" id="share-icon"></i>
-                                <ul class="action-icons" id="action-icons">
-                                    <li onclick="copyLink()"><i class="fas fa-copy"></i></li>
-                                    <li onclick="downloadContent()"><i class="fas fa-download"></i></li>
-                                </ul>
+    <div class="container-fluid py-5 mt-5">
+        <div class="container-fluid py-5 mt-5">
+            <div class="container py-5">
+                <div class="row g-4 mb-5">
+                    <div class="col-lg-12">
+                        <div class="row g-4">
+                            <div class="col-lg-4">
+                                <div class="border rounded">
+                                    <a href="#">
+                                        <img src="img/single-item.jpg" class="img-fluid rounded" alt="Image">
+                                    </a>
+                                </div>
                             </div>
-                        </div>
-                        <p class="mb-3">Creador: {{$cliente->nickname}}</p>
+                            <div class="col-lg-8">
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <h4 class="fw-bold mb-3">{{$recipe->title_recipe}}</h4>
+                                    <div class="icon-container">
+                                        <i class="fas fa-heart icon"></i>
+                                        <i class="fas fa-share-alt icon" id="share-icon"></i>
+                                        <i class="fa fa-flag me-2 btn-report" id="reportBtn" title="Reportar receta" style="margin-left: 40px;"></i>
+                                        <ul class="action-icons" id="action-icons">
+                                            <li onclick="copyLink()"><i class="fas fa-copy"></i></li>
+                                            <li onclick="downloadContent()"><i class="fas fa-download"></i></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <p class="mb-3">Creador: {{$cliente->nickname}}</p>
                                 @if($recipe->privacy == "private")
-                                    <p class="mb-3"><b>Esta receta es privada.</b></p>
+                                <p class="mb-3"><b>Esta receta es privada.</b></p>
                                 @endif
                                 <div class="d-flex mb-3 ">
-                                    @for ($i = 1; $i <= 5; $i++)
-                                        @if ($i <= $recipe->comentarios()->get()->avg('puntuacion')+0.49) <!--Para que se vaya al siguiente entero  a partir del x.51-->
-                                            <i class="fa fa-star text-secondary"></i> 
+                                    @for ($i = 1; $i <= 5; $i++) @if ($i <=$recipe->comentarios()->get()->avg('puntuacion')+0.49) <!--Para que se vaya al siguiente entero  a partir del x.51-->
+                                        <i class="fa fa-star text-secondary"></i>
                                         @else
-                                            <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
                                         @endif
-                                    @endfor
+                                        @endfor
                                 </div>
-                        <p class="mb-4">{{$recipe->descrip_recipe}}</p>
-                    </div>
+                                <p class="mb-4">{{$recipe->descrip_recipe}}</p>
+                            </div>
 
-                    <nav>
-                        <div class="nav nav-tabs mb-3">
-                        </div>
-                    </nav>
-                    <div class="col-lg-5">
-                        <h4 class="fw-bold mb-3">Ingredientes</h4>
-                        <ol>
-                            @foreach($recipe->ingredients as $ingredientes)
-                                <li>{{$ingredientes->ingredient}}</li>
-                            @endforeach
-                            <!--<li>Primer Ingrediente</li>
+                            <nav>
+                                <div class="nav nav-tabs mb-3">
+                                </div>
+                            </nav>
+                            <div class="col-lg-5">
+                                <h4 class="fw-bold mb-3">Ingredientes</h4>
+                                <ol>
+                                    @foreach($recipe->ingredients as $ingredientes)
+                                    <li>{{$ingredientes->ingredient}}</li>
+                                    @endforeach
+                                    <!--<li>Primer Ingrediente</li>
                             <li>Segundo Ingrediente</li>
                             <li>Tercer Ingrediente</li>
                             <li>Cuarto Ingrediente</li>
                             <li>Quinto Ingrediente</li>
                             <li>Sexto Ingrediente</li>-->
-                        </ol>
-                        
-                    </div>
-                    <div class="col-lg-7">
-                        <h4 class="fw-bold mb-3">Instrucciones</h4>
-                        <ol>
-                        @foreach($recipe->instructions as $instrucciones)
-                            <li>{{$instrucciones->instruccion}}</li>
-                        @endforeach
-                        </ol>
-                       <!-- <p class="mb-4">The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
+                                </ol>
+
+                            </div>
+                            <div class="col-lg-7">
+                                <h4 class="fw-bold mb-3">Instrucciones</h4>
+                                <ol>
+                                    @foreach($recipe->instructions as $instrucciones)
+                                    <li>{{$instrucciones->instruccion}}</li>
+                                    @endforeach
+                                </ol>
+                                <!-- <p class="mb-4">The generated Lorem Ipsum is therefore always free from repetition injected humour, or non-characteristic words etc.</p>
                         <p class="mb-4">Susp endisse ultricies nisi vel quam suscipit. Sabertooth peacock flounder; chain pickerel hatchetfish, pencilfish snailfish</p>
 -->
-                    </div>
-
-
-                    <nav>
-                        <div class="nav nav-tabs mb-3">
-                        </div>
-                    </nav>
-
-                    <form action="{{route('comentario.store',$recipe->id)}}" method="POST" class="col-lg-12">
-                        @csrf
-                        <input type="hidden" name="receta_id" value="{{ $recipe->id }}"> 
-                        <h4 class="mb-5 fw-bold">Añade un comentario</h4>
-                                <div class="border-bottom rounded my-4">
-                            <textarea name="comentario" id="comentario" class="form-control border-0" cols="30" rows="8" placeholder="Your Review *" spellcheck="false"></textarea>
-                                </div>
-
-                        <h4 class="mb-3 fw-bold">Valoración:</h4>
-                        <p class="clasificacion">
-                            <input id="radio1" type="radio" name="estrellas" value="1" class="d-none"/>
-                            <label for="radio1"><i class="fa fa-star text-secondary"></i></label>
-                            <input id="radio2" type="radio" name="estrellas" value="2" class="d-none"/>
-                            <label for="radio2"><i class="fa fa-star text-secondary"></i></label>
-                            <input id="radio3" type="radio" name="estrellas" value="3" class="d-none"/>
-                            <label for="radio3"><i class="fa fa-star text-secondary"></i></label>
-                            <input id="radio4" type="radio" name="estrellas" value="4" class="d-none"/>
-                            <label for="radio4"><i class="fa fa-star text-secondary"></i></label>
-                            <input id="radio5" type="radio" name="estrellas" value="5" class="d-none"/>
-                            <label for="radio5"><i class="fa fa-star text-secondary"></i></label>
-                        </p>
-
-                        <div class="mb-4">
-                            <button type="submit" class="btn btn-primary">Enviar Comentario</button>
                             </div>
-                    </form>
+
 
                             <nav>
                                 <div class="nav nav-tabs mb-3">
-                            <h4 class="mb-5 fw-bold">Comentario</h4>
-                            @if ($errors->any())
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
                                 </div>
-                            @endif   
+                            </nav>
+
+                            <form action="{{route('comentario.store',$recipe->id)}}" method="POST" class="col-lg-12">
+                                @csrf
+                                <input type="hidden" name="receta_id" value="{{ $recipe->id }}">
+                                <h4 class="mb-5 fw-bold">Añade un comentario</h4>
+                                <div class="border-bottom rounded my-4">
+                                    <textarea name="comentario" id="comentario" class="form-control border-0" cols="30" rows="8" placeholder="Your Review *" spellcheck="false"></textarea>
+                                </div>
+
+                                <h4 class="mb-3 fw-bold">Valoración:</h4>
+                                <p class="clasificacion">
+                                    <input id="radio1" type="radio" name="estrellas" value="1" class="d-none" />
+                                    <label for="radio1"><i class="fa fa-star text-secondary"></i></label>
+                                    <input id="radio2" type="radio" name="estrellas" value="2" class="d-none" />
+                                    <label for="radio2"><i class="fa fa-star text-secondary"></i></label>
+                                    <input id="radio3" type="radio" name="estrellas" value="3" class="d-none" />
+                                    <label for="radio3"><i class="fa fa-star text-secondary"></i></label>
+                                    <input id="radio4" type="radio" name="estrellas" value="4" class="d-none" />
+                                    <label for="radio4"><i class="fa fa-star text-secondary"></i></label>
+                                    <input id="radio5" type="radio" name="estrellas" value="5" class="d-none" />
+                                    <label for="radio5"><i class="fa fa-star text-secondary"></i></label>
+                                </p>
+
+                                <div class="mb-4">
+                                    <button type="submit" class="btn btn-primary">Enviar Comentario</button>
+                                </div>
+                            </form>
+
+                            <nav>
+                                <div class="nav nav-tabs mb-3">
+                                    <h4 class="mb-5 fw-bold">Comentario</h4>
+                                    @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                    @endif
                                 </div>
                             </nav>
                             @foreach($recipe->comentarios()->get() as $comentar)
@@ -128,16 +127,15 @@
                                         <div class="d-flex justify-content-between">
                                             <h5 style="padding-right:20px;">{{$comentar->user->nickname}}</h5>
                                             <div class="d-flex mb-3 ">
-                                                @for ($i = 0; $i < 5; $i++)
-                                                    @if ($i < $comentar->puntuacion)
-                                                        <i class="fa fa-star text-secondary"></i> 
+                                                @for ($i = 0; $i < 5; $i++) @if ($i < $comentar->puntuacion)
+                                                    <i class="fa fa-star text-secondary"></i>
                                                     @else
-                                                        <i class="fa fa-star"></i>
+                                                    <i class="fa fa-star"></i>
                                                     @endif
-                                                @endfor
+                                                    @endfor
                                             </div>
                                         </div>
-                                            <p>{{$comentar->comentario}}</p>
+                                        <p>{{$comentar->comentario}}</p>
                                     </div>
                                 </div>
                             </div>
@@ -148,19 +146,49 @@
             </div>
         </div>
     </div>
-</div>
-<!-- Single Product End -->
-<script>
-document.querySelectorAll('.clasificacion input').forEach(input => {
-    input.addEventListener('change', function() {
-        let clickedRating = this.value;
-        document.querySelectorAll('.clasificacion label i').forEach((icon, index) => {
-            icon.className = (index >= clickedRating) ? 'fa fa-star' : 'fa fa-star text-secondary';
+    </div>
+
+    <!-- Modal -->
+    <div id="reportModal" class="modal">
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <h4>Motivo</h4>
+            <textarea id="motivo" class="form-control" rows="4" placeholder="Escribe el motivo del reporte" style="resize: none"></textarea>
+            <button id="sendReport" class="btn-send mt-3">Enviar</button>
+        </div>
+    </div>
+    <!-- Single Product End -->
+    <script>
+        document.querySelectorAll('.clasificacion input').forEach(input => {
+            input.addEventListener('change', function() {
+                let clickedRating = this.value;
+                document.querySelectorAll('.clasificacion label i').forEach((icon, index) => {
+                    icon.className = (index >= clickedRating) ? 'fa fa-star' : 'fa fa-star text-secondary';
+                });
+            });
         });
-    });
-});
-</script>
-<script>
+
+        document.getElementById('reportBtn').addEventListener('click', function() {
+            document.getElementById('reportModal').style.display = 'flex';
+        });
+
+        document.querySelector('.close').addEventListener('click', function() {
+            document.getElementById('reportModal').style.display = 'none';
+        });
+
+        window.addEventListener('click', function(event) {
+            if (event.target == document.getElementById('reportModal')) {
+                document.getElementById('reportModal').style.display = 'none';
+            }
+        });
+
+        document.getElementById('sendReport').addEventListener('click', function() {
+            // Aquí puedes agregar la lógica para enviar el reporte
+            alert('Reporte enviado: ' + document.getElementById('motivo').value);
+            document.getElementById('reportModal').style.display = 'none';
+        });
+    </script>
+    <script>
         document.getElementById('share-icon').onclick = function(event) {
             var icons = document.getElementById('action-icons');
             icons.style.display = icons.style.display === 'block' ? 'none' : 'block';
@@ -175,8 +203,7 @@ document.querySelectorAll('.clasificacion input').forEach(input => {
         };
 
         function copyLink() {
-            navigator.clipboard.writeText('{{url()->current()}}').then(function() {
-            }).catch(function(error) {
+            navigator.clipboard.writeText('{{url()->current()}}').then(function() {}).catch(function(error) {
                 alert('Error al copiar el enlace');
             });
         }
